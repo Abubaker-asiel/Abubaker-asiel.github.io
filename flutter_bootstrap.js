967bd07482183478534898e77b020063c8c7c1e0
abubaker ahmed <abubakerahmed@abubakers-MacBook-Air.local>
2026-06-10 03:32:48 +0300

    adding animations to the app

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1012531864"
+    serviceWorkerVersion: "707838851"
   }
 });
