e4ad53dd81973bdbf9d9e6c953a526f1e3368ed2
abubaker ahmed <abubakerahmed@abubakers-MacBook-Air.local>
2026-06-18 14:51:15 +0300

    updaing site mangmenget

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"1527ae0ec577a4ef50e65f6fefcfc1326707d9
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1875744199"
+    serviceWorkerVersion: "1876602661"
   }
 });
